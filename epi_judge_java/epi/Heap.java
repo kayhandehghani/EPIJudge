f536b3007b878ab20e4f362251528878c5395e2f
I852958 <kayhan.dehghani.mohammadi@sap.com>
2019-07-26 15:57:07 -0700

    Add top k using heap

---
+++ epi_judge_java/epi/Heap.java
@@ -0,0 +1,52 @@
+package epi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Heap {
+	
+	public static List<String> topK(int k, Iterator<String> iter) {
+		int count = 0;
+		
+		if (k <= 0) {
+			return new ArrayList<String>();
+		}
+		
+		PriorityQueue<String> minHeap = new PriorityQueue<>((s1, s2) -> s1.length() - s2.length());
+		
+		while(iter.hasNext()) {
+			String curr = iter.next();
+			
+			if (minHeap.peek() != null && minHeap.peek().length() >= curr.length() && minHeap.size() >= k) {
+				continue; // improving performance
+			}
+			
+			minHeap.add(curr);
+			count++;
+			if (minHeap.size() > k) {
+				minHeap.poll();
+			}
+		}
+		System.out.println("Number of insertion was: " + count);
+		
+		return new ArrayList<>(minHeap);
+	}
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>(Arrays.asList(new String[]{"a", "b", "", "aa", "bb", "abc", "cddd", "cddd", "", "aaaaa", "a"}));
+		
+		System.out.println(topK(-1, list.iterator()));
+		System.out.println(topK(0, list.iterator()));
+		System.out.println(topK(1, list.iterator()));
+		System.out.println(topK(2, list.iterator()));
+		System.out.println(topK(3, list.iterator()));
+		System.out.println(topK(4, list.iterator()));
+		System.out.println(topK(5, list.iterator()));
+		System.out.println(topK(list.size(), list.iterator()));
+		System.out.println(topK(list.size() + 1, list.iterator()));
+	}
+
+}
