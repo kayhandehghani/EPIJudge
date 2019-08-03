f71936595d543ed2c9e08a1e647fbe803109414e
I852958 <kayhan.dehghani.mohammadi@sap.com>
2019-08-02 17:03:54 -0700

    Add fib and max sub array using DP

--- epi_judge_java/epi/Fibonacci.java
+++ epi_judge_java/epi/Fibonacci.java
@@ -3,18 +3,36 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class Fibonacci {
 
-  @EpiTest(testDataFile = "fibonacci.tsv")
+	@EpiTest(testDataFile = "fibonacci.tsv")
 
-  public static int fibonacci(int n) {
-    // TODO - you fill in here.
-    return -1;
-  }
+	public static int fibonacci(int n) {
+		return fibonacciIterativeDP(n);
+	}
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Fibonacci.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	// O(n) time and O(1) space
+	public static int fibonacciIterativeDP(int n) {
+		if (n <= 1) {
+			return n;
+		}
+
+		int nMinus1 = 1;
+		int nMinus2 = 0;
+		int result = 1;
+
+		for (int i = 2; i <= n; i++) {
+			result = nMinus1 + nMinus2;
+			nMinus2 = nMinus1;
+			nMinus1 = result;
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+				.runFromAnnotations(args, "Fibonacci.java",
+						new Object() {}.getClass().getEnclosingClass())
+				.ordinal());
+	}
 }
--- epi_judge_java/epi/MaxSumSubarray.java
+++ epi_judge_java/epi/MaxSumSubarray.java
@@ -1,20 +1,28 @@
 package epi;
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
 public class MaxSumSubarray {
-  @EpiTest(testDataFile = "max_sum_subarray.tsv")
+	@EpiTest(testDataFile = "max_sum_subarray.tsv")
 
-  public static int findMaximumSubarray(List<Integer> A) {
-    // TODO - you fill in here.
-    return -1;
-  }
+	public static int findMaximumSubarray(List<Integer> A) {
+		int minSum = 0, sumToHere = 0, maxSum = 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "MaxSumSubarray.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+		for (int a : A) {
+			sumToHere += a;
+			minSum = Math.min(minSum, sumToHere);
+			maxSum = Math.max(maxSum, sumToHere - minSum);
+		}
+
+		return maxSum;
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+				.runFromAnnotations(args, "MaxSumSubarray.java",
+						new Object() {}.getClass().getEnclosingClass())
+				.ordinal());
+	}
 }
