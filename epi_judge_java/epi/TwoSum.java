0f63f0f6ce953ea5432100a55e193f9a09d93868
I852958 <kayhan.dehghani.mohammadi@sap.com>
2019-08-06 10:05:31 -0700

    Add two solutions for hasSumTwo, using greedy with/without invariants

--- epi_judge_java/epi/TwoSum.java
+++ epi_judge_java/epi/TwoSum.java
@@ -1,20 +1,56 @@
 package epi;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
 public class TwoSum {
-  @EpiTest(testDataFile = "two_sum.tsv")
+	@EpiTest(testDataFile = "two_sum.tsv")
 
-  public static boolean hasTwoSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
-  }
+	public static boolean hasTwoSum(List<Integer> A, int t) {
+		//		Collections.sort(A, Collections.reverseOrder()); // turning on reverse sort breaks approach one
+		//		return approach2(A, t);
+		return approach1(A, t);
+	}
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TwoSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	// time: O(n), space: O(1) but only works with sorted array
+	public static boolean approach2(List<Integer> A, int t) {
+		int l = 0, h = A.size() - 1;
+
+		while (l <= h) {
+			if (A.get(l) + A.get(h) == t) {
+				return true;
+			} else if (A.get(l) + A.get(h) < t) {
+				l++;
+			} else {
+				h--;
+			}
+		}
+
+		return false;
+	}
+
+	// time: O(n), space: O(n) but works with unsorted array as well
+	public static boolean approach1(List<Integer> A, int t) {
+		Map<Integer, Integer> valueIndexMap = new HashMap<>();
+
+		for (int i = 0; i < A.size(); i++) {
+			int curr = A.get(i);
+			if (t - curr == curr || valueIndexMap.containsKey(t - curr)) {
+				return true;
+			}
+			valueIndexMap.put(curr, i);
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+				.runFromAnnotations(args, "TwoSum.java",
+						new Object() {}.getClass().getEnclosingClass())
+				.ordinal());
+	}
 }
