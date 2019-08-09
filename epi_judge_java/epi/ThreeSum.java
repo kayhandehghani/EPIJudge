65054ca3a5243302c1cfa9935647cac9d3da15d9
I852958 <kayhan.dehghani.mohammadi@sap.com>
2019-08-09 10:01:41 -0700

    Add hasThreeSum

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -1,20 +1,29 @@
 package epi;
+import java.util.Collections;
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
 public class ThreeSum {
-  @EpiTest(testDataFile = "three_sum.tsv")
+	@EpiTest(testDataFile = "three_sum.tsv")
 
-  public static boolean hasThreeSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
-  }
+	public static boolean hasThreeSum(List<Integer> A, int t) {
+		Collections.sort(A);
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ThreeSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+		for (int a : A) {
+			if (TwoSum.hasTwoSum(A, t - a)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+				.runFromAnnotations(args, "ThreeSum.java",
+						new Object() {}.getClass().getEnclosingClass())
+				.ordinal());
+	}
 }
