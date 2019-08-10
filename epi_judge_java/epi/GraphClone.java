86b0751144e6aa4eaef03b095a7a43c4cdfd3b3a
I852958 <kayhan.dehghani.mohammadi@sap.com>
2019-08-09 21:39:19 -0700

    Add clone graph and number of traversal in 2D map

--- epi_judge_java/epi/GraphClone.java
+++ epi_judge_java/epi/GraphClone.java
@@ -1,107 +1,131 @@
 package epi;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
 public class GraphClone {
 
-  public static class GraphVertex {
-    public int label;
-    public List<GraphVertex> edges;
+	public static class GraphVertex {
+		public int label;
+		public List<GraphVertex> edges;
 
-    public GraphVertex(int label) {
-      this.label = label;
-      edges = new ArrayList<>();
-    }
-  }
+		public GraphVertex(int label) {
+			this.label = label;
+			edges = new ArrayList<>();
+		}
+	}
 
-  public static GraphVertex cloneGraph(GraphVertex graph) {
-    // TODO - you fill in here.
-    return new GraphVertex(0);
-  }
-  private static List<Integer> copyLabels(List<GraphVertex> edges) {
-    List<Integer> labels = new ArrayList<>();
-    for (GraphVertex e : edges) {
-      labels.add(e.label);
-    }
-    return labels;
-  }
+	public static GraphVertex cloneGraph(GraphVertex graph) {
+		if (graph == null) {
+			return null;
+		}
 
-  private static void checkGraph(GraphVertex node, List<GraphVertex> graph)
-      throws TestFailure {
-    if (node == null) {
-      throw new TestFailure("Graph was not copied");
-    }
+		Map<GraphVertex, GraphVertex> originalCloneMap = new HashMap<>();
+		Queue<GraphVertex> queue = new LinkedList<>();
 
-    Set<GraphVertex> vertexSet = new HashSet<>();
-    Queue<GraphVertex> q = new ArrayDeque<>();
-    q.add(node);
-    vertexSet.add(node);
-    while (!q.isEmpty()) {
-      GraphVertex vertex = q.remove();
-      if (vertex.label >= graph.size()) {
-        throw new TestFailure("Invalid vertex label");
-      }
-      List<Integer> label1 = copyLabels(vertex.edges),
-                    label2 = copyLabels(graph.get(vertex.label).edges);
-      Collections.sort(label1);
-      Collections.sort(label2);
-      if (!label1.equals(label2)) {
-        throw new TestFailure("Edges mismatch");
-      }
-      for (GraphVertex e : vertex.edges) {
-        if (!vertexSet.contains(e)) {
-          vertexSet.add(e);
-          q.add(e);
-        }
-      }
-    }
-  }
+		queue.add(graph);
+		originalCloneMap.put(graph, new GraphVertex(graph.label));
 
-  @EpiUserType(ctorParams = {int.class, int.class})
-  public static class Edge {
-    public int from;
-    public int to;
+		while (!queue.isEmpty()) {
+			GraphVertex currentOriginal = queue.poll();
 
-    public Edge(int from, int to) {
-      this.from = from;
-      this.to = to;
-    }
-  }
+			for (GraphVertex e : currentOriginal.edges) {
+				if (!originalCloneMap.containsKey(e)) {
+					originalCloneMap.put(e, new GraphVertex(e.label));
+					queue.add(e);
+				}
+				originalCloneMap.get(currentOriginal).edges.add(originalCloneMap.get(e));
+			}
+		}
 
-  @EpiTest(testDataFile = "graph_clone.tsv")
-  public static void cloneGraphTest(int k, List<Edge> edges)
-      throws TestFailure {
-    if (k <= 0) {
-      throw new RuntimeException("Invalid k value");
-    }
-    List<GraphVertex> graph = new ArrayList<>();
-    for (int i = 0; i < k; i++) {
-      graph.add(new GraphVertex(i));
-    }
-    for (Edge e : edges) {
-      if (e.from < 0 || e.from >= k || e.to < 0 || e.to >= k) {
-        throw new RuntimeException("Invalid vertex index");
-      }
-      graph.get(e.from).edges.add(graph.get(e.to));
-    }
-    GraphVertex result = cloneGraph(graph.get(0));
-    checkGraph(result, graph);
-  }
+		return originalCloneMap.get(graph);
+	}
+	private static List<Integer> copyLabels(List<GraphVertex> edges) {
+		List<Integer> labels = new ArrayList<>();
+		for (GraphVertex e : edges) {
+			labels.add(e.label);
+		}
+		return labels;
+	}
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "GraphClone.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	private static void checkGraph(GraphVertex node, List<GraphVertex> graph)
+			throws TestFailure {
+		if (node == null) {
+			throw new TestFailure("Graph was not copied");
+		}
+
+		Set<GraphVertex> vertexSet = new HashSet<>();
+		Queue<GraphVertex> q = new ArrayDeque<>();
+		q.add(node);
+		vertexSet.add(node);
+		while (!q.isEmpty()) {
+			GraphVertex vertex = q.remove();
+			if (vertex.label >= graph.size()) {
+				throw new TestFailure("Invalid vertex label");
+			}
+			List<Integer> label1 = copyLabels(vertex.edges),
+					label2 = copyLabels(graph.get(vertex.label).edges);
+			Collections.sort(label1);
+			Collections.sort(label2);
+			if (!label1.equals(label2)) {
+				throw new TestFailure("Edges mismatch");
+			}
+			for (GraphVertex e : vertex.edges) {
+				if (!vertexSet.contains(e)) {
+					vertexSet.add(e);
+					q.add(e);
+				}
+			}
+		}
+	}
+
+	@EpiUserType(ctorParams = {int.class, int.class})
+	public static class Edge {
+		public int from;
+		public int to;
+
+		public Edge(int from, int to) {
+			this.from = from;
+			this.to = to;
+		}
+	}
+
+	@EpiTest(testDataFile = "graph_clone.tsv")
+	public static void cloneGraphTest(int k, List<Edge> edges)
+			throws TestFailure {
+		if (k <= 0) {
+			throw new RuntimeException("Invalid k value");
+		}
+		List<GraphVertex> graph = new ArrayList<>();
+		for (int i = 0; i < k; i++) {
+			graph.add(new GraphVertex(i));
+		}
+		for (Edge e : edges) {
+			if (e.from < 0 || e.from >= k || e.to < 0 || e.to >= k) {
+				throw new RuntimeException("Invalid vertex index");
+			}
+			graph.get(e.from).edges.add(graph.get(e.to));
+		}
+		GraphVertex result = cloneGraph(graph.get(0));
+		checkGraph(result, graph);
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+				.runFromAnnotations(args, "GraphClone.java",
+						new Object() {}.getClass().getEnclosingClass())
+				.ordinal());
+	}
 }
--- epi_judge_java/epi/NumberOfTraversalsMatrix.java
+++ epi_judge_java/epi/NumberOfTraversalsMatrix.java
@@ -1,19 +1,73 @@
 package epi;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class NumberOfTraversalsMatrix {
-  @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
+	@EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
 
-  public static int numberOfWays(int n, int m) {
-    // TODO - you fill in here.
-    return 0;
-  }
+	public static int numberOfWays(int n, int m) {
+		return numberOfWays(n, m, new HashMap<Cell, Integer>());
+	}
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NumberOfTraversalsMatrix.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	private static int numberOfWays(int n, int m, Map<Cell, Integer> cellCount) {
+		if (n <= 0 || m <= 0) {
+			return 0;
+		} else if (n == 1 && m == 1) {
+			cellCount.put(new Cell(0, 0), 1);
+			return 1;
+		} else if (cellCount.containsKey(new Cell(n, m))) {
+			return cellCount.get(new Cell(n, m));
+		} else {
+			int fromLeft =  numberOfWays(n - 1, m, cellCount);
+			int fromTop =  numberOfWays(n, m - 1, cellCount);
+			cellCount.put(new Cell(n - 1, m), fromLeft);
+			cellCount.put(new Cell(n, m - 1), fromTop);
+			return fromLeft + fromTop;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.exit(GenericTest.runFromAnnotations(args, "NumberOfTraversalsMatrix.java", new Object() {
+		}.getClass().getEnclosingClass()).ordinal());
+	}
+
+}
+
+class Cell {
+	int n;
+	int m;
+
+	public Cell(int n, int m) {
+		this.n = n;
+		this.m = m;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + m;
+		result = prime * result + n;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cell other = (Cell) obj;
+		if (m != other.m)
+			return false;
+		if (n != other.n)
+			return false;
+		return true;
+	}
 }
