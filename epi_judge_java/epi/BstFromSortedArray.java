155943b7d4356a18c16af1bd311524607f80baa7
I852958 <kayhan.dehghani.mohammadi@sap.com>
2019-08-31 12:51:03 -0700

    Add create Min Height BST from sorted array

--- epi_judge_java/epi/BstFromSortedArray.java
+++ epi_judge_java/epi/BstFromSortedArray.java
@@ -1,37 +1,44 @@
 package epi;
+
+import java.util.List;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import epi.test_framework.TestFailure;
 import epi.test_framework.TestUtils;
 import epi.test_framework.TimedExecutor;
-import java.util.List;
+
 public class BstFromSortedArray {
 
-  public static BstNode<Integer>
-  buildMinHeightBSTFromSortedArray(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "bst_from_sorted_array.tsv")
-  public static int
-  buildMinHeightBSTFromSortedArrayWrapper(TimedExecutor executor,
-                                          List<Integer> A) throws Exception {
-    BstNode<Integer> result =
-        executor.run(() -> buildMinHeightBSTFromSortedArray(A));
+	public static BstNode<Integer> buildMinHeightBSTFromSortedArray(List<Integer> A) {
+		return addNodesRecursively(0, A.size(), A);
+	}
 
-    List<Integer> inorder = BinaryTreeUtils.generateInorder(result);
+	private static BstNode<Integer> addNodesRecursively(int l, int r, List<Integer> A) {
+		if (l >= r) {
+			return null;
+		}
 
-    TestUtils.assertAllValuesPresent(A, inorder);
-    BinaryTreeUtils.assertTreeIsBst(result);
-    return BinaryTreeUtils.binaryTreeHeight(result);
-  }
+		int m = l + ((r - l) / 2);
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BstFromSortedArray.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+		return new BstNode<>(A.get(m), addNodesRecursively(l, m, A), addNodesRecursively(m + 1, r, A));
+
+	}
+
+	@EpiTest(testDataFile = "bst_from_sorted_array.tsv")
+	public static int buildMinHeightBSTFromSortedArrayWrapper(TimedExecutor executor, List<Integer> A)
+			throws Exception {
+		BstNode<Integer> result = executor.run(() -> buildMinHeightBSTFromSortedArray(A));
+
+		List<Integer> inorder = BinaryTreeUtils.generateInorder(result);
+
+		TestUtils.assertAllValuesPresent(A, inorder);
+		BinaryTreeUtils.assertTreeIsBst(result);
+		return BinaryTreeUtils.binaryTreeHeight(result);
+	}
+
+	public static void main(String[] args) {
+		System.exit(GenericTest.runFromAnnotations(args, "BstFromSortedArray.java", new Object() {
+		}.getClass().getEnclosingClass()).ordinal());
+	}
 }
